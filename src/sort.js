c046f7ea70289f078b9a5bc1fb2327fa7b1ab3c1
youjunho613 <njn613@gmail.com>
2023-06-06 16:09:06 +0900

    feat. 정렬 기능
    메인페이지 영화카드 인기순, 이름순, 개봉순으로 정렬하는 radio type input
    추가

--- src/sort.js
+++ src/sort.js
@@ -5,20 +5,18 @@ sortMenu.addEventListener("change", () => {
   drawSortMovieList(event);
 });
 
-const radio1 = document.querySelector("#radio1");
-const radio2 = document.querySelector("#radio2");
-const radio3 = document.querySelector("#radio3");
-
 const drawSortMovieList = async event => {
   const movieList = await getPopularMovieList();
-  const movieListElement = document.querySelector(".movie-list");
-  if (event.target === radio1) {
+
+  if (event.target === document.querySelector("#radio1")) {
     movieList.sort((a, b) => b.vote_average - a.vote_average);
-  } else if (event.target === radio2) {
+  } else if (event.target === document.querySelector("#radio2")) {
     movieList.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (event.target === radio3) {
+  } else if (event.target === document.querySelector("#radio3")) {
     movieList.sort((a, b) => b.release_date.localeCompare(a.release_date));
   }
+
+  const movieListElement = document.querySelector(".movie-list");
   movieListElement.innerHTML = movieList.reduce((newMovieList, movieItem) => {
     const { poster_path, title, overview, vote_average, id } = movieItem;
 
