9701b35573a41d8acd9328cf4b779d21836804aa
youjunho613 <njn613@gmail.com>
2023-06-07 21:06:13 +0900

    feat. 현재상영영화 get&draw, 정렬 기능 추가
    메인페이지 현재상영 영화 추가
    정렬 기능 코드 수정

--- src/movie.js
+++ src/movie.js
@@ -1,4 +1,4 @@
-import { getPopularMovieList, getSearchMovieList } from "./apis/movie.js";
+import { getPopularMovieList, getSearchMovieList, getNowPlayingMovie } from "./apis/movie.js";
 
 export const drawMovieList = movieList => {
   const movieListElement = document.querySelector(".movie-list");
@@ -23,10 +23,35 @@ export const drawMovieList = movieList => {
   }, "");
 };
 
+export const drawNowMovieList = nowMovieList => {
+  const movieListElement = document.querySelector(".now-movie-list");
+
+  movieListElement.innerHTML = nowMovieList.reduce((newMovieList, movieItem) => {
+    const { poster_path, title, overview, vote_average, id } = movieItem;
+
+    return (newMovieList += `
+          <li class="movie-item" id=${id}>
+              <div class="item-poster">
+                <div class="poster-movie">
+                  <img src="https://image.tmdb.org/t/p/w200/${poster_path}" alt="${title}" />
+                  <span class="movie-rating">Rating : ${vote_average}</span>
+                </div>
+                <div class="poster-info">
+                </div>
+              </div>
+              <h2 class="movie-title">${title}</h2>
+              <p class="movie-desc" style="display: none;">${overview}</p>
+          </li>
+        `);
+  }, "");
+};
+
 export const renderPopularMovie = async () => {
   const movieList = await getPopularMovieList();
+  const nowMovieList = await getNowPlayingMovie();
 
   drawMovieList(movieList);
+  drawNowMovieList(nowMovieList);
 };
 
 export const renderSearchMovie = async () => {
@@ -36,7 +61,7 @@ export const renderSearchMovie = async () => {
   const searchMovieList = await getSearchMovieList(searchKeyword);
 
   if (searchMovieList.length > 0) {
-    drawMovieList(searchMovieList);
+    drawNowMovieList(searchMovieList);
   } else {
     alert("검색된 결과가 없습니다.");
   }
--- src/sort.js
+++ src/sort.js
@@ -1,40 +1,26 @@
-import { getPopularMovieList } from "./apis/movie.js";
+import { getNowPlayingMovie } from "./apis/movie.js";
+import { drawNowMovieList } from "./movie.js";
 
-const sortMenu = document.querySelector("#sortMenu");
+const sortMenu = document.querySelector("#sortOpt");
 sortMenu.addEventListener("change", () => {
-  drawSortMovieList(event);
+  drawSortMovieList();
 });
 
-const drawSortMovieList = async event => {
-  const movieList = await getPopularMovieList();
+const drawSortMovieList = async () => {
+  const movieList = await getNowPlayingMovie();
+  const sortOpt = document.querySelector("#sortOpt");
 
-  //스위치문으로 변경
-  if (event.target === document.querySelector("#radio1")) {
-    movieList.sort((a, b) => b.vote_average - a.vote_average);
-  } else if (event.target === document.querySelector("#radio2")) {
-    movieList.sort((a, b) => a.title.localeCompare(b.title));
-  } else if (event.target === document.querySelector("#radio3")) {
-    movieList.sort((a, b) => b.release_date.localeCompare(a.release_date));
+  switch (sortOpt.value) {
+    case "vote":
+      movieList.sort((a, b) => b.vote_average - a.vote_average);
+      break;
+    case "title":
+      movieList.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case "day":
+      movieList.sort((a, b) => b.release_date.localeCompare(a.release_date));
+      break;
   }
 
-  // drawMovieList 함수 사용 import
-  const movieListElement = document.querySelector(".movie-list");
-  movieListElement.innerHTML = movieList.reduce((newMovieList, movieItem) => {
-    const { poster_path, title, overview, vote_average, id } = movieItem;
-
-    return (newMovieList += `
-    <li class="movie-item" id=${id}>
-        <div class="item-poster">
-          <div class="poster-movie">
-            <img src="https://image.tmdb.org/t/p/w200/${poster_path}" alt="${title}" />
-            <span class="movie-rating">Rating : ${vote_average}</span>
-          </div>
-          <div class="poster-info">
-          </div>
-        </div>
-        <h2 class="movie-title">${title}</h2>
-        <p class="movie-desc" style="display: none;">${overview}</p>
-    </li>
-        `);
-  }, "");
+  drawNowMovieList(movieList);
 };
